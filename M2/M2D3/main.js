14fb520a45d25c159efabdbcfd2b392a3bf1070b
andrewb1991 <andrea.bramucci@gmail.com>
2022-11-01 18:56:58 +0100

    01 11

---
+++ M2/M2D3/main.js
@@ -0,0 +1,23 @@
+/* FOR LOOPS */
+
+// for (let i = 2; i<=10; i++) {
+// console.log(`Value of i:${i}`);
+// }
+
+/* BREAK AND CONTINUE */ 
+
+for (let i = 0; i<10; i++) {
+    console.log(`Value of i:${i}`);
+    }
+    if (i === 5) {
+    break;
+    }
+
+
+    // for (let i = 0; i<10; i++) {
+    //     console.log(`Value of i:${i}`);
+    //     }
+    // if (i === 5) {
+    //     continue
+    //     }
+    
