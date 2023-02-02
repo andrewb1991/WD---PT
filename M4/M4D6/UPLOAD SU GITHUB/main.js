6118fb17ba672adc5776ce768524e3f12bd6a166
andrewb1991 <andrea.bramucci@gmail.com>
2023-02-02 12:45:01 +0100

    2 2 23 solo

---
+++ M4/M4D6/UPLOAD SU GITHUB/main.js
@@ -0,0 +1,34 @@
+const getData = async (url) => {
+    try {
+        const data = await fetch(url)
+        return await data.json();
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+getData("https://jsonplaceholder.typicode.com/users").then((res)=>{
+console.log(res)
+res.map(data =>{
+const name = data.name
+const username = data.username
+const email = data.email
+const addName = document.createElement("h5")
+addName.innerText = `${data.name}`
+console.log(name)  
+const card = document.getElementById("newCard")
+const addUsername = document.createElement("h5")
+addUsername.innerText = `${data.username}`
+console.log(username)
+console.log(email)  
+const addEmail = document.createElement("h5")
+addEmail.innerText = `${data.email}`
+const mainCard = document.getElementById("mainCard")
+const div = document.createElement("div")
+div.style.border = "2px solid yellow"
+div.style.width = "200px"
+mainCard.append(div)
+div.append(card, addName, addUsername, addEmail)
+});
+})
