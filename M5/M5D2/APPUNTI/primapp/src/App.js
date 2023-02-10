40ae901584fa43beb64a8d47cbf8c9898ebee455
andrewb1991 <andrea.bramucci@gmail.com>
2023-02-10 16:48:13 +0100

    eliminate

--- M5/M5D2/APPUNTI/primapp/src/App.js
+++ M5/M5D2/APPUNTI/primapp/src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-         <Button variant="danger">AIUTO</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,9 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-   
-  )
-  ;
+  );
 }
 
 export default App;
