b8cd8cc6f85748060ae720e5fa5f741662e16b02
andrewb1991 <andrea.bramucci@gmail.com>
2023-03-11 10:50:52 +0100

    PRO
    PROV

--- M6/M6D1/APPUNTI/SERVEREPIBOOKS/middlewares/logMiddlewares.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-
-const logMiddelwares = (req, res, next) =>{
-    console.log(`ricevuta chiamata ${req.method}all'endpoint ${req.originalUrl}`)
-    next();
-}
-
-module.exports = logMiddelwares
