0ed886f17c0116b5d33af1384e8816340540bd84
andrewb1991 <andrea.bramucci@gmail.com>
2023-03-13 18:53:41 +0100

    13 3 23 solo

--- M6/M6D1/APPUNTI/SERVEREPIBOOKS/routes/blogpost.js
+++ M6/M6D1/APPUNTI/SERVEREPIBOOKS/routes/blogpost.js
@@ -21,15 +21,15 @@ router.post("/BlogPosts", async(req, res)=>{
         cover: req.body.cover,
         readTime: {
             metadata: {
-                value: req.body.readTime.value,
-                unit: req.body.readTime.unit,  
-            }
+                value: req.body.readTime.metadata.value,
+                unit: req.body.readTime.metadata.unit,  
+            },
         },
         author: {
             metadata: {
-                name: req.body.author.name,
-                avatar: req.body.author.avatar,
-            }
+                name: req.body.author.metadata.name,
+                avatar: req.body.author.metadata.avatar,
+            },
         },
         content: req.body.content
     })
