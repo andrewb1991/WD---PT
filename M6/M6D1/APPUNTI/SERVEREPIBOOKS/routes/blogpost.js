1511f763e1392b1514358ad52b17624c7bfada12
andrewb1991 <andrea.bramucci@gmail.com>
2023-03-14 17:25:37 +0100

    14 3 23 solo - post new blog internal server error

--- M6/M6D1/APPUNTI/SERVEREPIBOOKS/routes/blogpost.js
+++ M6/M6D1/APPUNTI/SERVEREPIBOOKS/routes/blogpost.js
@@ -15,18 +15,26 @@ router.get("/BlogPosts", async(req, res)=>{
 })
 
 router.post("/BlogPosts", async(req, res)=>{
+    // const readTimeValue = req.body.readTime.value
+    // const readTimeUnit = req.body.readTime.unit
+    // const authorName = req.body.author.name
+    // const authorAvatar = req.body.author.avatar
     const blogpost = new BlogPosts({
         category: req.body.category,
         title: req.body.title,
         cover: req.body.cover,
-        readTime:{
-            value: req.body.readTime.value,
-            unit: req.body.readTime.unit   
-            },
-        author: {
-                name: req.body.author.name,
-                avatar: req.body.author.avatar
-        },
+        // readTimeValue: req.body.readTime.value,
+        // readTimeUnit: req.body.readTime.unit,
+        // authorName: req.body.author.name,
+        // authorAvatar: req.body.author.avatar,
+        // readTime:{
+        //     value: req.body.readTime.value,
+        //     unit: req.body.readTime.unit   
+        //     },
+        // author: {
+        //         name: req.body.author.name,
+        //         avatar: req.body.author.avatar
+        // },
         content: req.body.content
     })
     try {
@@ -44,10 +52,10 @@ router.post("/BlogPosts", async(req, res)=>{
 
     })
 
-router.get("BlogPosts/:_id", async(req, res)=>{
-    const {_id} = req.params
+router.get("/BlogPosts/:id", async(req, res)=>{
+    const {id} = req.params
     try {
-        const blogpost = await BlogPosts.findById(_id)
+        const blogpost = await BlogPosts.findById(id)
         if(!blogpost){
         return res.status(404).send({ message: "BlogPost non trovato"})
         }
@@ -60,6 +68,50 @@ router.get("BlogPosts/:_id", async(req, res)=>{
     }
 })
 
+router.patch("/BlogPosts/:id", async(req, res)=>{
+    const {id} = req.params
+    const blogpostExist = await BlogPosts.findById(id)
+    if(!blogpostExist){
+        res.status(400).send({
+        message: "Blogpost not found"
+        })
+    }
+    try {
+        const blogpostToUpdate = req.body
+        const options = {
+        new: true
+        }
+    const result = await BlogPosts.findByIdAndUpdate(id, blogpostToUpdate, options)
+    res.status(200).send({
+        message: "Blogpost updated!",
+        payload: result
+    })
+    } catch (error) {
+        res.status(500).send({
+        message: "Internal server error",
+        error: error
+        })
+    }
+}
+)
 
-
+router.delete("/BlogPosts/:id", async(req, res)=>{
+    const {id} = req.params
+    try {
+        const blogpost = await BlogPosts.findById(id).deleteOne()
+        if(!blogpost){
+            return res.status(404).send({
+                message: "Blogpost not found"
+            })
+        }
+    res.status(200).send({
+        message: "Blogpost deleted"
+    })
+    } catch (error) {
+        res.status(500).send({
+        message: "Internal server error",
+        error: error
+        })
+    }
+})
 module.exports = router
--- M6/M6D1/UPLOAD SU GITHUB/striveblog/striveblog/src/views/new/New.jsx
+++ M6/M6D1/UPLOAD SU GITHUB/striveblog/striveblog/src/views/new/New.jsx
@@ -4,18 +4,77 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "./styles.css";
 const NewBlogPost = props => {
-  const [text, setText] = useState("");
+  const [newtitle, setTitle] = useState("");
+  const [newcategory, setCategory] = useState("");
+  const [newcover, setCover] = useState("");
+  const [newcontent, setContent] = useState("");
+  const [message, setMessage] = useState("");
+  const [readTimeValue, setReadTimeValue] = useState("");
+  const [readTimeUnit, setReadTimeUnit] = useState("");
+  const [authorName, setAuthorName] = useState("");
+  const [authorAvatar, setAuthorAvatar] = useState("");
+
   const handleChange = useCallback(value => {
-    setText(value);
+    setContent(value);
+    setTitle(value);
+    setCategory(value)
+    setCover(value)
+    setReadTimeValue(value)
+    setReadTimeUnit(value)
+    setAuthorName(value)
+    setAuthorAvatar(value)
   });
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:3030/BlogPosts/", {
+        method: "POST",
+        body: JSON.stringify({
+          title: newtitle,
+          category: newcategory,
+          cover: newcover,
+          content: newcontent,
+          // readTimeValue: readTimeValue,
+          // readTimeUnit: readTimeUnit,
+          // authorName: authorName,
+          // authorAvatar: authorAvatar,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setTitle("");
+        setCategory("");
+        setCover("");
+        setContent("");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
   return (
     <Container className="new-blog-container">
       <Form className="mt-5">
         <Form.Group controlId="blog-form" className="mt-3">
           <Form.Label>Titolo</Form.Label>
-          <Form.Control size="lg" placeholder="Title" />
+          <Form.Control type="text" value={newtitle} onChange={(e)=> setTitle(e.target.value)} size="lg" placeholder="Inserisci Titolo..." />
+          <Form.Label>Categoria</Form.Label>
+          <Form.Control type="text" value={newcategory} onChange={(e)=> setCategory(e.target.value)} size="lg" placeholder="inserisci Categoria..." /> <Form.Label>Cover</Form.Label>
+          <Form.Control type="text" value={newcover} onChange={(e)=> setCover(e.target.value)} size="lg" placeholder="Inserisci URL Immagine" />
+          {/* <Form.Label>Lettura in: </Form.Label>
+          <Form.Control type="text" value={readTimeValue} onChange={(e)=> setReadTimeValue(e.target.value)} size="lg" placeholder="inserisci Categoria..." />          <Form.Label>Unità</Form.Label>
+          <Form.Control type="text" value={readTimeUnit} onChange={(e)=> setReadTimeUnit(e.target.value)} size="lg" placeholder="inserisci Categoria..." />          <Form.Label>Nome dell'Autore</Form.Label>
+          <Form.Control type="text" value={authorName} onChange={(e)=> setAuthorName(e.target.value)} size="lg" placeholder="inserisci Categoria..." />          <Form.Label>Avatar dell'autore</Form.Label>
+          <Form.Control type="text" value={authorAvatar} onChange={(e)=> setAuthorAvatar(e.target.value)} size="lg" placeholder="inserisci Categoria..." /> */}
+          <Form.Control type="text" value={newcontent} onChange={(e)=> setContent(e.target.value)} size="lg" placeholder="Inserisci il contenuto del BlogPost" />
+          
         </Form.Group>
-        <Form.Group controlId="blog-category" className="mt-3">
+
+        {/* <Form.Group controlId="blog-category" className="mt-3">
           <Form.Label>Categoria</Form.Label>
           <Form.Control size="lg" as="select">
             <option>Categoria 1</option>
@@ -27,13 +86,13 @@ const NewBlogPost = props => {
         </Form.Group>
         <Form.Group controlId="blog-content" className="mt-3">
           <Form.Label>Contenuto Blog</Form.Label>
-          <ReactQuill value={text} onChange={handleChange} className="new-blog-content" />
-        </Form.Group>
+          <ReactQuill value={content} onChange={(e)=> setContent(e.target.value)} className="new-blog-content" />
+        </Form.Group> */}
         <Form.Group className="d-flex mt-3 justify-content-end">
           <Button type="reset" size="lg" variant="outline-dark">
             Reset
           </Button>
-          <Button
+          <Button onClick={handleSubmit}
             type="submit"
             size="lg"
             variant="dark"
